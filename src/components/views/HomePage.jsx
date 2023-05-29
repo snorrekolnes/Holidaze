f1a003b87864a71524b77eb70cba61f0b973acef
snorre kolnes røgenes <snorre.kolnes@hotmail.com>
2023-05-29 18:08:36 +0200

    name

--- src/components/views/HomePage.jsx
+++ src/components/views/HomePage.jsx
@@ -13,7 +13,7 @@ function Home() {
   const CHARACTER_LIMIT = 20;
   const {name, token} = useSelector(state => state.auth)
   console.log(name, token)
-  const welcomeName =   JSON.parse(name)
+ // const welcomeName =   JSON.parse(name)
     const dispatch = useDispatch();
     const {venues} = useSelector(state => state.venues);
 
@@ -38,7 +38,7 @@ function Home() {
              <div className='hero mt-36  flex flex-col text-center object-cover h-96 w-full bg-red-600'>
              <div className='max-w-sm items-center m-auto'>
             <h1 className="my-4  text-5xl max-w-sm m-auto tablet:max-w-md laptop:max-w-lg text-white font-bold leading-tight">
-               Welcome Back {welcomeName}
+               Welcome Back {name}
              </h1>
              <h1 className="my-22 text-3xl max-w-sm tablet:max-w-md laptop:max-w-lg m-auto text-white font-bold leading-tight">
                Feeling Lucky?
